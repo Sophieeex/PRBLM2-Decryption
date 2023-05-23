ba8d505cec6c10f6e7409c439cedfb5dbfe5107a
Your Name <you@example.com>
2023-05-23 08:38:59 +0800

    input function

--- redo-decryption.py
+++ redo-decryption.py
@@ -3,5 +3,8 @@
 # Laboratory Program: Assignment #3: decryption
 
 # collect string input from user 
+
+plain_text = input("Type your string input: ")
+
 # dictionary for converting special characters into vowels
 # checking each character and printing an output
