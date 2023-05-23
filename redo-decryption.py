b065aec1eb999a4af617e9f8aa64572f4666b476
Your Name <you@example.com>
2023-05-23 08:37:14 +0800

    Pseudocode

---
+++ redo-decryption.py
@@ -0,0 +1,7 @@
+# Name: Sophia Marie T. Reyes
+# Year & Section: BSCpE 1-5
+# Laboratory Program: Assignment #3: decryption
+
+# collect string input from user 
+# dictionary for converting special characters into vowels
+# checking each character and printing an output
